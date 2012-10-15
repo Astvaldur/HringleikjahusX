7d70f8fe5da4691fc44496a8fa0048d484fae638
Astvaldur <Astvaldur@130.208.101.75>
2012-10-15 10:15:59 +0000

    Commit með öllu

---
+++ src/com/example/colosseum/ConnectOther.java
@@ -0,0 +1,11 @@
+package com.example.colosseum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectOther extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout. connectother_view);
+	 }
+	}
---
+++ src/com/example/colosseum/User.java
@@ -0,0 +1,12 @@
+package com.example.colosseum;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class User extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_view);
+
+	 }
+	}
